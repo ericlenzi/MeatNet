420575f4bb697a9db2d5f6617594864b4e6b4a81
Eric Lenzi <eric.lenzi@sbi-technology.com>
2026-05-29 12:41:03 -0300

    Deleted dapper project

--- source/api/Meat.Queries/Dtos/ArticuloPrecioFallidoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Meat.Queries.Dtos
-{
-    public class ArticuloPrecioFallidoDto
-    {
-        public string Codigo { get; set; }
-        public string NumeroSucursal { get; set; }
-        public string Error { get; set; }
-    }
-}
--- source/api/Meat.Queries/Dtos/Response/ArticuloMarcasResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Meat.Queries.Dtos.Response
-{
-    public class ArticuloMarcasResponse
-    {
-        public IEnumerable<ArticulosMarcasDto> Marcas { get; set; }
-    }
-}
